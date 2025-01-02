58d486e2a80101baa7eb0c471ddc1934acad01a4
Your Name <ngoducnghia01648927528@gmail.com>
2025-01-02 22:48:41 +0700

    deploy: change vercel config

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Install Flutter
 FLUTTER_DIR="/opt/flutter"
 if [ ! -d "$FLUTTER_DIR" ]; then
+  echo "Installing Flutter..."
   git clone https://github.com/flutter/flutter.git -b stable $FLUTTER_DIR
+else
+  echo "Flutter is already installed."
 fi
 
 export PATH="$FLUTTER_DIR/bin:$PATH"
-
-# Run flutter doctor to ensure Flutter is ready
 flutter doctor
+which flutter
