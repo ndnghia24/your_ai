d9e498411b4a0ce51a8ab0196343c482eefcabfa
ndnghia24 <ndnghia24@users.noreply.github.com>
2025-07-14 04:48:01 +0000

    deploy: ef4fa431a627e640ff3ab83ada94aa26b53b7dd3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "628f358936608cc9752e8a62691967c4
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "eaeda24da11655acb78f188cee885532",
 "version.json": "fdbd2d2fe9a8a07dedd7c9ae868881e0",
 "manifest.json": "4c85ce0ee378ecca105713c6e39c080f",
-"flutter_bootstrap.js": "134afc70f312569c36de5bc252042af2",
+"flutter_bootstrap.js": "b7b97a73726cace1328ca3cbf09a28fb",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
