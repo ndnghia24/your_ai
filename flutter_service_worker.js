675f1d1d7059853558442c2c7ab6f1aa66753a36
ndnghia24 <ndnghia24@users.noreply.github.com>
2026-08-10 05:39:05 +0000

    deploy: ef4fa431a627e640ff3ab83ada94aa26b53b7dd3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "c3d4e65bcf898838b62c9c84ad4cdc48",
+"flutter_bootstrap.js": "ad1e3dcc39f1ffb3d863031c1b1ab946",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
