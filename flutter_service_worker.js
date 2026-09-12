44cb11d98250c94ee33270393bbf82563a29a8c1
ndnghia24 <ndnghia24@users.noreply.github.com>
2026-09-12 08:33:34 +0000

    deploy: ef4fa431a627e640ff3ab83ada94aa26b53b7dd3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"manifest.json": "4c85ce0ee378ecca105713c6e39c080f",
 "version.json": "fdbd2d2fe9a8a07dedd7c9ae868881e0",
 "main.dart.js": "5fce08c52024b7e160e6574607c10a8a",
-"flutter_bootstrap.js": "33939706f5d996b5f9e61a15b5ea177b",
+"flutter_bootstrap.js": "3f317a3c7a0825993e134768cec2aa4e",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
